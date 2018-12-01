97e932f3ca8db91e1e9860dfd97f94f5079d398c
Jamessom Queiroz <jamessomqueiroz@gmail.com>
2018-12-01 16:19:44 -0200

    add: database config env

--- config.js
+++ config.js
@@ -18,6 +18,13 @@ const config = {
   },
   server: {
     port: Number(process.env.PORT)
+  },
+  databse: {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE_NAME,
+    port: process.env.DATABASE_PORT
   }
 }
 
